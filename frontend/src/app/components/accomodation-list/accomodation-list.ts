22bf5b4031b6501432278e6128064df1d1f95efb
Karwan Karasool Hasan Bndyan <k.bndyan@itbh.local>
2025-06-30 12:29:39 +0200

    creating components-1

---
+++ frontend/src/app/components/accomodation-list/accomodation-list.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-accomodation-list',
+  imports: [],
+  templateUrl: './accomodation-list.html',
+  standalone: true,
+  styleUrl: './accomodation-list.scss'
+})
+export class AccomodationList {
+
+}
