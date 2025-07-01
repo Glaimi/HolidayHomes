f54e101c8e3ce01d772f0955f9241f665acf9a76
Mirko <mfoerster.hb@proton.me>
2025-07-01 09:03:08 +0200

    Refactor: Remove unneeded references between models

--- backend/Data/Models/AccommodationType.cs
+++ backend/Data/Models/AccommodationType.cs
@@ -6,8 +6,4 @@ public class AccommodationType
 {
     public int Id { get; set; }
     [MaxLength(255)] public string? Title { get; set; }
-
-    //                 1 : n
-    // AccommodationType : Accommodation
-    public List<Accommodation> Accommodations { get; set; } = new List<Accommodation>();
 }
