e4d699243b2ed8d2bcb27c94b43fcedd476ccc6a
Patrick <Patrick.Wenskowski@itbildungshauseducation.onmicrosoft.com>
2025-07-01 09:05:51 +0200

    accomodation is showing

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import {AccomodationList}  from './components/accomodation-list/accomodation-list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'accomodation-list',
+    component: AccomodationList,
+    title: 'list of accomodaions'
+  }
+];
--- frontend/src/app/components/accomodation-list/accomodation-list.ts
+++ frontend/src/app/components/accomodation-list/accomodation-list.ts
@@ -1,4 +1,5 @@
-import { Component } from "@angular/core";
+import {Component, Input} from "@angular/core";
+import {AccomodationModel} from '../../interfaces/accomodation-model';
 
 @Component({
   selector: 'app-accomodation-list',
@@ -8,5 +9,5 @@ import { Component } from "@angular/core";
   styleUrl: './accomodation-list.scss'
 })
 export class AccomodationList {
-
+@Input() accomodation!: AccomodationModel;
 }
