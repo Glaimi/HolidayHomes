d4a54332c1c957a99cad42269346a56843fe6cd1
Karwan Karasool Hasan Bndyan <k.bndyan@itbh.local>
2025-07-03 16:26:16 +0200

    squashed features 1, 58, 52

--- frontend/src/app/accomodation/accomodation.ts
+++ frontend/src/app/accomodation/accomodation.ts
@@ -1,15 +1,43 @@
-import {Component, Input} from '@angular/core';
-import {AccomodationModel} from '../interfaces/accomodation-model';
-import {RouterLink} from '@angular/router';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { 
+  faMapMarkerAlt, 
+  faBed, 
+  faExpand, 
+  faWifi, 
+  faParking, 
+  faHome,
+  faHeart as faSolidHeart
+} from '@fortawesome/free-solid-svg-icons';
+import { faHeart as faRegularHeart } from '@fortawesome/free-regular-svg-icons';
+import { AccomodationModel } from '../interfaces/accomodation-model';
 
 @Component({
   selector: 'app-accomodation',
-  imports: [],
-  templateUrl: './accomodation.html',
   standalone: true,
+  imports: [CommonModule, RouterLink, FontAwesomeModule],
+  templateUrl: './accomodation.html',
   styleUrl: './accomodation.scss'
 })
 export class Accomodation {
   @Input() accomodation!: AccomodationModel;
-
+  
+  // Font Awesome Icons
+  faMapMarkerAlt = faMapMarkerAlt;
+  faBed = faBed;
+  faExpand = faExpand;
+  faWifi = faWifi;
+  faParking = faParking;
+  faHome = faHome;
+  faSolidHeart = faSolidHeart;
+  faRegularHeart = faRegularHeart;
+  
+  isFavorite = false;
+  
+  toggleFavorite(event: Event) {
+    event.stopPropagation();
+    this.isFavorite = !this.isFavorite;
+  }
 }
