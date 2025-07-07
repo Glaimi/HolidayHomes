d2bc9159e2072c37a6f4a2a2199faefa33522b65
Karwan Bndyan <karwan.bndyan@itbildungshauseducation.onmicrosoft.com>
2025-07-07 11:18:46 +0000

    Feature: Startpage is completed

--- frontend/src/app/services/accomodation-service.ts
+++ frontend/src/app/services/accomodation-service.ts
@@ -13,4 +13,11 @@ export class AccomodationService {
   getAllAccomodations(): Observable<AccomodationModel[]> {
     return this.http.get<AccomodationModel[]>('http://localhost:5152/api/Accommodation');
   }
+
+  getAccomodationIdImage(id: number): Observable<string[]> {
+    return this.http.get<string[]>(`http://localhost:5152/api/accommodations/${id}/images`);
+  }
+
+  //getImageForCardInAccommodationList(accommodationId: number): Observable<ImageModel[]> {
+   // return this.http.get<ImageModel[]>(`http://localhost:5152/api/accommodations/${accommodationId}/images`);}
 }
