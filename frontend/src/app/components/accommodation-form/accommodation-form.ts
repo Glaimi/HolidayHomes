e2dc4f84cb52b7ba1152e28bae85eb427c70c122
Mirko <mfoerster.hb@proton.me>
2025-07-10 11:08:24 +0200

    Feature: Display all needed input fields in form

--- frontend/src/app/components/accommodation-form/accommodation-form.ts
+++ frontend/src/app/components/accommodation-form/accommodation-form.ts
@@ -6,6 +6,8 @@ import {AsyncPipe} from '@angular/common';
 import {FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
 import {AccommodationTypeService} from '../../services/accommodation-type-service';
 import {AccommodationTypeModel} from '../../interfaces/accommodation-type-model';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {AccommodationModel} from '../../interfaces/accommodation-model';
 
 @Component({
   selector: 'app-accommodation-form',
@@ -17,6 +19,9 @@ import {AccommodationTypeModel} from '../../interfaces/accommodation-type-model'
   styleUrl: './accommodation-form.scss'
 })
 export class AccommodationForm implements OnInit {
+  private http: HttpClient = inject(HttpClient);
+  private baseUrl: string = 'http://localhost:5152/api/Accommodation';
+
   // Needed for retrieving and displaying available accommodation types in the form
   private accommodationTypeService: AccommodationTypeService = inject(AccommodationTypeService);
   public accommodationTypes$!: Observable<AccommodationTypeModel[]>;
@@ -36,7 +41,15 @@ export class AccommodationForm implements OnInit {
     numberOfBedrooms: new FormControl(0),
     numberOfLivingRooms: new FormControl(0),
     numberOfMixedRooms: new FormControl(0),
+    numberOfBathtubs: new FormControl(0),
+    numberOfShowers: new FormControl(0),
     kitchenType: new FormControl(1),
+    seasonABookable: new FormControl(false),
+    seasonAPrice: new FormControl(0),
+    seasonBBookable: new FormControl(false),
+    seasonBPrice: new FormControl(0),
+    seasonCBookable: new FormControl(false),
+    seasonCPrice: new FormControl(0),
     shortTripAvailability: new FormControl(0),
     bedsheetsAvailability: new FormControl(0),
     towelsAvailability: new FormControl(0),
@@ -44,6 +57,7 @@ export class AccommodationForm implements OnInit {
     isDogsAllowed: new FormControl(false),
     isNonSmoking: new FormControl(false),
     isTelevisionAvailable: new FormControl(false),
+    isWashingMachineAvailable: new FormControl(false),
     isParkingAvailable: new FormControl(false),
     isSaunaAvailable: new FormControl(false),
     hints: new FormControl('')
@@ -59,6 +73,10 @@ export class AccommodationForm implements OnInit {
   public maxNumberOfLivingRooms: number = 8;
   public minNumberOfMixedRooms: number = 0;
   public maxNumberOfMixedRooms: number = 8;
+  public minNumberOfBathtubs: number = 0;
+  public maxNumberOfBathtubs: number = 4;
+  public minNumberOfShowers: number = 0;
+  public maxNumberOfShowers: number = 4;
 
   ngOnInit(): void {
     this.accommodationTypes$ = this.accommodationTypeService.getAllAccommodationTypes();
@@ -66,10 +84,53 @@ export class AccommodationForm implements OnInit {
   }
 
   onSubmit(): void {
+    console.log(this.formGroup.value);
+
     if (this.formGroup.valid) {
-      console.log(this.formGroup.value);
+      const options = {
+        headers: new HttpHeaders({
+          'Content-Type': 'application/json'
+        })
+      };
+
+      const accommodationDto = {
+        accommodationTypeId: this.formGroup.get('accommodationType')?.value,
+        kitchenTypeId: this.formGroup.get('kitchenType')?.value,
+        name: this.formGroup.get('accommodationName')?.value,
+        street: this.formGroup.get('street')?.value,
+        city: this.formGroup.get('city')?.value,
+        hints: this.formGroup.get('hints')?.value,
+        landLordName: this.formGroup.get('landlordName')?.value,
+        squareMeter: this.formGroup.get('squareMeters')?.value,
+        numberOfBedrooms: this.formGroup.get('numberOfBedrooms')?.value,
+        numberOfBeds: this.formGroup.get('numberOfBeds')?.value,
+        numberOfMixedRooms: this.formGroup.get('numberOfMixedRooms')?.value,
+        numberOfLivingRooms: this.formGroup.get('numberOfLivingRooms')?.value,
+        isDogAllowed: this.formGroup.get('isDogsAllowed')?.value,
+        isWifiAvailable: this.formGroup.get('isWifiAvailable')?.value,
+        isNonSmoking: this.formGroup.get('isNonSmoking')?.value,
+        isTelevisionAvailable: this.formGroup.get('isTelevisionAvailable')?.value,
+        isWashingMachineAvailable: this.formGroup.get('isWashingMachineAvailable')?.value,
+        isParkingAvailable: this.formGroup.get('isParkingAvailable')?.value,
+        isSaunaAvailable: this.formGroup.get('isSaunaAvailable')?.value,
+        bedSheetsAvailability: this.formGroup.get('bedsheetsAvailability')?.value,
+        shortTripAvailability: this.formGroup.get('shortTripAvailability')?.value,
+        towelsAvailability: this.formGroup.get('towelsAvailability')?.value,
+        accommodationSanitaryInfos: this.getSanitaryInfos()
+      };
+
+      this.http.post<AccommodationModel>(this.baseUrl, accommodationDto, options).subscribe(accommodation => {
+        console.log(accommodation);
+      });
     } else {
-      console.log("Ungültige Formulardaten!");
+      console.warn('Unvollständige Formulardaten');
     }
+  }
+
+  getSanitaryInfos(): { sanitaryTypeId: number, amount: number}[] {
+    return [
+      { sanitaryTypeId: 1, amount: this.formGroup.get('numberOfShowers')?.value ?? 0 },
+      { sanitaryTypeId: 2, amount: this.formGroup.get('numberOfBathtubs')?.value ?? 0 }
+    ];
   }
 }
