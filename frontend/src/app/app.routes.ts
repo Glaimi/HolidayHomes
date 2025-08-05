c87540a679854f19af0fcfce10a25ed73d106ae6
Patrick Wenskowski <patrick.wenskowski@itbildungshauseducation.onmicrosoft.com>
2025-08-05 06:59:57 +0000

    Feature 94 95

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import {Imprint} from './components/imprint/imprint';
 import {AccommodationList} from './components/accommodation-list/accommodation-list';
 import {Privacy} from './components/privacy/privacy';
 import {AccommodationDetails} from './components/accommodation-details/accommodation-details';
+import {ManageAvailability} from './components/manage-availability/manage-availability';
 
 
 export const routes: Routes = [
@@ -31,6 +32,11 @@ export const routes: Routes = [
     path:'view-details/:id',
     component: AccommodationDetails,
     title: 'Details anzeigen'
+  },
+  {
+    path:'manageAvailability',
+    component: ManageAvailability,
+    title: 'Verfügbarkeit verwalten'
   },
   {
     path: '**', redirectTo: '',
