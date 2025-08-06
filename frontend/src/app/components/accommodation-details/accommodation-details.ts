1d771f8d987f517037977017ba50801de57f3afc
Goekhan <goekhan.demirkaya@itbildungshauseducation.onmicrosoft.com>
2025-08-06 11:02:08 +0200

    booking-service.ts and changes

--- frontend/src/app/components/accommodation-details/accommodation-details.ts
+++ frontend/src/app/components/accommodation-details/accommodation-details.ts
@@ -1,24 +1,15 @@
 import { Component, inject } from '@angular/core';
 import {Observable} from 'rxjs';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 import {AccommodationService} from '../../services/accommodation-service';
 import {AccommodationModel} from '../../interfaces/accommodation-model';
-import {AsyncPipe, DecimalPipe, NgForOf, NgIf} from '@angular/common';
+import {AsyncPipe, DatePipe, DecimalPipe, NgForOf, NgIf} from '@angular/common';
 import {ImageModel} from '../../interfaces/image-model';
-import {
-  DateRange,
-  MatCalendar,
-  MatDatepickerActions,
-  MatDatepickerApply,
-  MatDatepickerCancel, MatDatepickerControl, MatDatepickerModule, MatDatepickerPanel,
-  MatDateRangePicker
-} from '@angular/material/datepicker';
-import {MatButton} from '@angular/material/button';
-import {MatNativeDateModule} from '@angular/material/core';
-import {MatFormField, MatHint, MatLabel, MatSuffix} from '@angular/material/input';
+import { MatCalendar } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 import {FormsModule} from '@angular/forms';
-
-
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-view.details',
@@ -27,23 +18,10 @@ import {FormsModule} from '@angular/forms';
     NgIf,
     NgForOf,
     DecimalPipe,
+    DatePipe,
     MatCalendar,
-    MatDateRangePicker,
-    MatDatepickerActions,
-    MatButton,
-    MatDatepickerCancel,
-    MatDatepickerApply,
-    MatDatepickerModule,
     MatNativeDateModule,
-    MatFormField,
-    MatLabel,
-    MatHint,
-    MatSuffix,
-    MatLabel,
     FormsModule
-  ],
-  providers:[
-    MatDatepickerModule,
   ],
   templateUrl: './accommodation-details.html',
   standalone: true,
@@ -57,21 +35,50 @@ export class AccommodationDetails {
   placeholderImage = 'placeholder.jpg';
   accommodationId:number;
 
-  constructor() {
+  constructor(private router: Router) {
     this.accommodationId = this.route.snapshot.params['id'];
     this.accommodationDetails$ = this.accommodationService.getAccommodationById(this.accommodationId);
     this.accommodationImages$ = this.accommodationService.getAccommodationIdImage(this.accommodationId);
   }
 
-  startDate!:Date;
-  endDate!:Date;
+  selectedDate: Date | null = null;
+  startDateSelected: Date | null = null;
+  endDateSelected: Date | null = null;
+
+  onDateSelected(date: Date | null) {
+    if (!date) return;
+    
+    if (!this.startDateSelected) {
+      // Erstes Datum wird als Startdatum gesetzt
+      this.startDateSelected = date;
+    } else if (!this.endDateSelected) {
+      // Zweites Datum wird als Enddatum gesetzt
+      if (date >= this.startDateSelected) {
+        this.endDateSelected = date;
+      } else {
+        // Falls das zweite Datum früher ist, tausche sie
+        this.endDateSelected = this.startDateSelected;
+        this.startDateSelected = date;
+      }
+    } else {
+      // Wenn beide Daten bereits gesetzt sind, beginne von vorne
+      this.startDateSelected = date;
+      this.endDateSelected = null;
+    }
+  }
+
+  resetSelection() {
+    this.startDateSelected = null;
+    this.endDateSelected = null;
+    this.selectedDate = null;
+  }
 
   dateRangeAusgeben(){
-    console.log("Startdatum", this.startDate);
-    console.log("Enddatum", this.endDate);
+    console.log("Startdatum", this.startDateSelected);
+    console.log("Enddatum", this.endDateSelected);
     console.log("ID", this.accommodationId);
 
-    this.accommodationService.bookAccommodation(this.accommodationId,this.startDate,this.endDate).subscribe(() => {
+    this.accommodationService.bookAccommodation(this.accommodationId,this.startDateSelected,this.endDateSelected).subscribe(() => {
       console.log("Abgesendet")
     })
   }
--- frontend/src/app/components/calendar/calendar.ts
+++ frontend/src/app/components/calendar/calendar.ts
@@ -1,22 +1,282 @@
-import {Component} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatNativeDateModule } from '@angular/material/core';
+import { FormsModule } from '@angular/forms';
+import { DateRange } from '@angular/material/datepicker';
+
+const now = new Date();
 
 @Component({
   selector: 'app-calendar',
-  imports: [],
+  imports: [
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatNativeDateModule,
+    FormsModule
+  ],
   templateUrl: './calendar.html',
   standalone: true,
-  styleUrl: './calendar.scss'
+  styleUrls: ['./calendar.scss']
 })
-export class Calendar {
-  //variable anlegen das das datum speichert/aufnimmt  eine für start eine für enddatum
-  //extra button mit jetzt buchen anlegen der funktion auslösen soll, wenn button geklickt, dann in konsole start und enddatum anzeigen soll
+export class Calendar implements OnInit {
+  startDate?: Date;
+  endDate?: Date;
 
-  startDate?:Date;
-  endDate?:Date;
+  min = '2025-08-06T00:00';
+  labels: any = [];
+  invalid: any = [];
+  colors: any = [];
+  monthColors = [
+    {
+      background: '#b2f1c080',
+      start: '2025-01-01T00:00',
+      end: '2025-01-31T00:00',
+      cellCssClass: 'md-book-rental-bg-off',
+      recurring: {
+        repeat: 'yearly',
+        month: 1,
+        day: 1,
+      },
+    },
+    {
+      background: '#b2f1c080',
+      start: '2025-02-01T00:00',
+      end: '2025-02-28T00:00',
+      cellCssClass: 'md-book-rental-bg-off',
+      recurring: {
+        repeat: 'yearly',
+        month: 2,
+        day: 1,
+      },
+    },
+    {
+      background: '#b2f1c080',
+      cellCssClass: 'md-book-rental-bg-off',
+      recurring: {
+        repeat: 'yearly',
+        month: 2,
+        day: 29,
+      },
+    },
+    {
+      background: '#a3cdff80',
+      start: '2025-03-01T00:00',
+      end: '2025-03-31T23:59',
+      cellCssClass: 'md-book-rental-bg-pre',
+      recurring: {
+        repeat: 'yearly',
+        month: 3,
+        day: 1,
+      },
+    },
+    {
+      background: '#a3cdff80',
+      start: '2025-04-01T00:00',
+      end: '2025-04-30T00:00',
+      cellCssClass: 'md-book-rental-bg-pre',
+      recurring: {
+        repeat: 'yearly',
+        month: 4,
+        day: 1,
+      },
+    },
+    {
+      background: '#a3cdff80',
+      start: '2025-05-01T00:00',
+      end: '2025-05-31T00:00',
+      cellCssClass: 'md-book-rental-bg-pre',
+      recurring: {
+        repeat: 'yearly',
+        month: 5,
+        day: 1,
+      },
+    },
+    {
+      background: '#f7f7bb80',
+      start: '2025-06-01T00:00',
+      end: '2025-06-30T00:00',
+      cellCssClass: 'md-book-rental-bg-in',
+      recurring: {
+        repeat: 'yearly',
+        month: 6,
+        day: 1,
+      },
+    },
+    {
+      background: '#f7f7bb80',
+      start: '2025-07-01T00:00',
+      end: '2025-07-31T00:00',
+      cellCssClass: 'md-book-rental-bg-in',
+      recurring: {
+        repeat: 'yearly',
+        month: 7,
+        day: 1,
+      },
+    },
+    {
+      background: '#f7f7bb80',
+      start: '2025-08-01T00:00',
+      end: '2025-08-31T00:00',
+      cellCssClass: 'md-book-rental-bg-in',
+      recurring: {
+        repeat: 'yearly',
+        month: 8,
+        day: 1,
+      },
+    },
+    {
+      background: '#f7f7bb80',
+      start: '2025-09-01T00:00',
+      end: '2025-09-30T00:00',
+      cellCssClass: 'md-book-rental-bg-in',
+      recurring: {
+        repeat: 'yearly',
+        month: 9,
+        day: 1,
+      },
+    },
+    {
+      background: '#f7f7bb80',
+      start: '2025-10-01T00:00',
+      end: '2025-10-31T23:59',
+      cellCssClass: 'md-book-rental-bg-in',
+      recurring: {
+        repeat: 'yearly',
+        month: 10,
+        day: 1,
+      },
+    },
+    {
+      background: '#b2f1c080',
+      start: '2025-11-01T00:00',
+      end: '2025-11-30T00:00',
+      cellCssClass: 'md-book-rental-bg-off',
+      recurring: {
+        repeat: 'yearly',
+        month: 11,
+        day: 1,
+      },
+    },
+    {
+      background: '#b2f1c080',
+      start: '2025-12-01T00:00',
+      end: '2025-12-31T00:00',
+      cellCssClass: 'md-book-rental-bg-off',
+      recurring: {
+        repeat: 'yearly',
+        month: 12,
+        day: 1,
+      },
+    },
+  ];
 
-  dateRangeAusgeben(){
-    console.log("Startdatum", this.startDate);
-    console.log("Enddatum", this.endDate);
+  displayedYear: number = now.getFullYear();
+  displayedMonth: number = now.getMonth();
+
+  // Für Angular Material Datepicker
+  minDate: Date = new Date();
+  maxDate: Date = new Date(new Date().getFullYear(), new Date().getMonth() + 6, new Date().getDate());
+
+  // Für den Date Range Picker
+  dateRange: DateRange<Date> = new DateRange<Date>(null, null);
+
+  constructor(private http: HttpClient) {}
+
+  getColors(start: Date, end: Date) {
+    return [
+      {
+        date: start,
+        cellCssClass: 'vacation-check-in',
+      },
+      {
+        date: end,
+        cellCssClass: 'vacation-check-out',
+      },
+      {
+        start: new Date(start.getFullYear(), start.getMonth(), start.getDate() + 1),
+        end: new Date(end.getFullYear(), end.getMonth(), end.getDate() - 1),
+        background: '#ffbaba80',
+        cellCssClass: 'vacation-booked',
+      },
+    ];
   }
 
+  ngOnInit(): void {
+    this.loadMonth(this.displayedYear, this.displayedMonth);
+  }
+
+  loadMonth(year: number, month: number) {
+    this.http.jsonp('https://trial.mobiscroll.com/getrentals/?year=' + year + '&month=' + month, 'callback').subscribe((data: any) => {
+      const prices = data.prices;
+      const bookings = data.bookings;
+      const labels: any = [];
+      const invalids: any = [];
+      let colors: any = [];
+      let endDate = new Date(year, month, 1, 0, 0);
+
+      for (const price of prices) {
+        const booked = bookings.find((b: { checkIn: any }) => {
+          const checkInDate = new Date(b.checkIn);
+          // YYYY-M-D Format erzeugen
+          const jsFormatted = `${checkInDate.getFullYear()}-${checkInDate.getMonth() + 1}-${checkInDate.getDate()}`;
+          return jsFormatted === price.date;
+        });
+        if (booked) {
+          const checkIn = new Date(booked.checkIn);
+          const checkOut = new Date(booked.checkOut);
+          const newCheckOut = new Date(checkOut.getFullYear(), checkOut.getMonth(), checkOut.getDate() - 1);
+          colors = [...colors, ...this.getColors(checkIn, checkOut)];
+          labels.push({
+            start: checkIn,
+            end: newCheckOut,
+            text: 'booked',
+            textColor: '#1e1e1ecc',
+          });
+          invalids.push({
+            start: checkIn,
+            end: newCheckOut,
+          });
+          endDate = checkOut;
+        } else if (new Date(price.date) >= endDate) {
+          labels.push({
+            date: new Date(price.date),
+            text: price.text,
+            textColor: price.textColor,
+          });
+        }
+      }
+      this.labels = labels;
+      this.invalid = invalids;
+      this.colors = [...colors, ...this.monthColors];
+    });
+  }
+
+  prevMonth() {
+    if (this.displayedMonth === 0) {
+      this.displayedMonth = 11;
+      this.displayedYear--;
+    } else {
+      this.displayedMonth--;
+    }
+    this.loadMonth(this.displayedYear, this.displayedMonth);
+  }
+
+  nextMonth() {
+    if (this.displayedMonth === 11) {
+      this.displayedMonth = 0;
+      this.displayedYear++;
+    } else {
+      this.displayedMonth++;
+    }
+    this.loadMonth(this.displayedYear, this.displayedMonth);
+  }
+
+  dateRangeAusgeben() {
+    console.log('Startdatum:', this.dateRange.start);
+    console.log('Enddatum:', this.dateRange.end);
+  }
 }
