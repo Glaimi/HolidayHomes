e9ff191e834c10ba8081785da53765429d98066b
Karwan Karasool Hasan Bndyan <k.bndyan@itbh.local>
2025-08-11 10:53:32 +0200

    Calculate and display price is finished

---
+++ backend/Business/Services/ISeasonPricingRepository.cs
@@ -0,0 +1,13 @@
+﻿//using Business.Dtos;
+//using Business.Mappers;
+//using Data.Models;
+//using Data.Repositories;
+
+
+namespace Business.Services
+{
+    public interface ISeasonPricingRepository
+    {
+        Task<IEnumerable<object>> GetByAccommodationIdAsync(int accommodationId);
+    }
+}
